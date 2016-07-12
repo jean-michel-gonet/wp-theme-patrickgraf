d747fc029a0799b137eeebdd8bc560da544df65e
jean-michel-gonet <root@agl-developpement.ch>
2016-07-12 18:52:15 +0000

    Nettoyage du style

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title><?php bloginfo(); ?></title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="robots" content="index, follow" />
 		<?php wp_head(); ?>
 		<link rel="icon" type="image/png" href="favicon.png" />		
